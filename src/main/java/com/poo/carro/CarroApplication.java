512545b6ce54c36924ee3097a86399f6445edb1f
Lucas Carvalho Lima <PET0105386@aluno.firjansenaisesi.com.br>
2023-08-21 11:58:58 -0300

    Aula

--- src/main/java/com/poo/carro/CarroApplication.java
+++ src/main/java/com/poo/carro/CarroApplication.java
@@ -2,9 +2,10 @@ package com.poo.carro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import com.poo.carro.classes.Conta;
+//import com.poo.carro.classes.Veiculo;
+import com.poo.carro.classes.Aluno;
 
-import com.poo.carro.classes.Conta;
-import com.poo.carro.classes.Veiculo;
 
 @SpringBootApplication
 public class CarroApplication {
@@ -12,25 +13,37 @@ public class CarroApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarroApplication.class, args);
 
-		Veiculo carroDoLucas = new Veiculo();
+		//Veiculo carroDoLucas = new Veiculo();
 		
-		carroDoLucas.proprietario = "Lucas";
-		carroDoLucas.renavam = "546545";
-		carroDoLucas.marca = "Fiat";
-		carroDoLucas.modelo = "Cayanne";
-		carroDoLucas.ano = 2023;
-		carroDoLucas.cor = "Preto";
-		carroDoLucas.quilometragem = 5000.0;
+		//carroDoLucas.proprietario = "Lucas";
+		//carroDoLucas.renavam = "546545";
+		//carroDoLucas.marca = "Fiat";
+		//carroDoLucas.modelo = "Cayanne";
+		//carroDoLucas.ano = 2023;
+		//carroDoLucas.cor = "Preto";
+		//carroDoLucas.quilometragem = 5000.0;
 
 		//carroDoLucas.imprimeDocumento();
 
-		Conta conta1 = new Conta ();
-		conta1.setSenha("1234Abc");
+		//Conta conta1 = new Conta ();
 
-		System.out.println(conta1.getSenha());
+		//System.out.println(conta1.getTitular());
+		//System.out.println(conta1.getSenha());
+		//System.out.println(conta1.getNumeroDaConta());
 
+		//Conta conta2 = new Conta ();
+		
+		//System.out.println(conta2.getTitular());
+		//System.out.println(conta2.getSenha());
+		//System.out.println(conta2.getNumeroDaConta());
 
+		Aluno inscricao	= new Aluno();
+		System.out.println("\nInscrição efetuada com sucesso!" +"\n" + "Nome: " + inscricao.getNome() + 
+		"\n" +"Data de nascimento: " + inscricao.getDataNasc() + "\nSua matrícula é: " + inscricao.getMatricula() + "\n");
 
+		Aluno inscricao2 = new Aluno();
+		System.out.println("\nInscrição efetuada com sucesso!" +"\n" + "Nome: " + inscricao2.getNome() + 
+		"\n" +"Data de nascimento: " + inscricao2.getDataNasc() + "\nSua matrícula é: " + inscricao2.getMatricula());
 
 	}
 
--- src/main/java/com/poo/carro/classes/Conta.java
+++ src/main/java/com/poo/carro/classes/Conta.java
@@ -1,32 +1,48 @@
 package com.poo.carro.classes;
 
+import java.util.Scanner;
+
 public class Conta {
     private String titular;
-    private String numeroDaConta;
+    private int numeroDaConta;
     private String senha;
+    static int contador = 1000;
+    
+
+    public Conta () {
+        System.out.println("Escreva o nome do titular: ");
+        Scanner leia = new Scanner(System.in);
+
+        this.setTitular(leia.nextLine());
+
+        this.setNumeroDaConta(contador);;
+        contador++;
+        leia.close();
+        this.senha = "1111";
+
+    }
 
     public String getTitular() {
         return titular;
     }
-
     public void setTitular(String titular) {
         this.titular = titular;
     }
-
-    public String getNumeroDaConta() {
+    public int getNumeroDaConta() {
         return numeroDaConta;
     }
-
-    public void setNumeroDaConta(String numeroDaConta) {
+    public void setNumeroDaConta(int numeroDaConta) {
         this.numeroDaConta = numeroDaConta;
     }
-
     public String getSenha() {
-        return this.senha;
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
     
-    public void setSenha(String novaSenha){
-        this.senha = novaSenha;
+            
     }
     
-}
+    
+
